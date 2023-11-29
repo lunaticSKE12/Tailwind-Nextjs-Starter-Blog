243571ed89ec581c64c726e98e7b5794c3c906f6
lunaticSKE12 <jamesnapongd@gmail.com>
2023-11-29 12:03:01 +0700

    Change color and logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,12 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+// import Logo from '@/data/logo.svg'
+import Logo from '@/data/logo.png'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import Image from 'next/image'
 
 const Header = () => {
   return (
@@ -12,8 +14,8 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
+            <div className="mr-3 w-12 md:w-16">
+              <Image src={Logo} alt="Lunatic" sizes="33vw" priority={true} />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
